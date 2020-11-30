f61bae2269eece991fb05ff1061e58a0b2b0320e
TGZafirov19 <64420385+TGZafirov19@users.noreply.github.com>
2020-11-30 20:20:56 +0200

    Try to add part of the code

--- math-game/math-game.cpp
+++ math-game/math-game.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-    
+    ofstream outputFile;
+	ifstream inputFile;
+	int correct = 0, wrong = 0;
+	double earning = 0.00;
+	string name, continu = "y";
+
+	unsigned seed = time(0);
+	srand(seed);
+
+	do
+	{
+		credits(continu);
+		if (continu == "y" || continu == "Y")
+		{
+			retrieveStats(outputFile, inputFile, name, correct, wrong, earning);
+			menu(name, correct, wrong, earning);
+			saveStats(outputFile, name, correct, wrong, earning);
+
+			correct = 0;
+			wrong = 0;
+			earning = 0.00;
+		}
+	} while (continu == "y" || continu == "Y");
+	return 0;
 }
